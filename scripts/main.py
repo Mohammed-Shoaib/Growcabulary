ded69aa51ec30e6509c8b60663ebe6281fe98950
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-09-06 06:24:12 +0400

    Restructure codebase with utils

--- scripts/main.py
+++ scripts/main.py
@@ -1,115 +1,9 @@
 import os
 import sys
-import cv2
-import json
 import shutil
 import argparse
 
-from typing import List
-from pathlib import Path
-from pprint import pprint
-
-
-
-def dict_raise_on_duplicates(ordered_pairs):
-	""" Raises an error for duplicate keys """
-	d = {}
-	for k, v in ordered_pairs:
-		if k in d and k != "":	# ! ignores empty keys
-		   raise ValueError(k)
-		else:
-		   d[k] = v
-	return d
-
-
-
-def pretty(key, value):
-	print(f'{key}: ', end='')
-	pprint(value)
-
-
-
-def get_data(data: dict):
-	words = {}
-	
-	
-	def run_tests(json_files: str) -> dict:
-		# check dictionary of data
-		for key, value in data.items():
-			if key in words:
-				pretty(key, words[key])
-				sys.exit(f"[\u2718] Duplicate key '{key}' found in file {file_path}.")
-		
-		# check if wordlist is sorted
-		pairs = []
-		keys = get_wordlist(data)
-		for i in range(1, len(keys)):
-			if keys[i - 1] > keys[i]:
-				pairs.append([keys[i - 1], keys[i]])
-		if pairs:
-			print(pairs)
-			sys.exit(f'[\u2718] Wordlist is not sorted for file {file_path}.')
-		
-		# check if image is present
-		images = set(os.listdir(os.path.join(base_dir, 'images')))
-		original = set(os.listdir(os.path.join(base_dir, 'original')))
-		no_image = []
-		for key, value in data.items():
-			found = False
-			for i in range(len(value)):
-				if value[i]['image'] in images and value[i]['image'] in original:
-					found = True
-			if not found:
-				no_image.append(key)
-		if no_image:
-			print(no_image)
-			sys.exit(f'[\u2718] Incorrect path to images for file {file_path}.')
-		
-		# check if images are square
-		not_square = []
-		for img_path in images:
-			img = cv2.imread(os.path.join(base_dir, 'images', img_path))
-			height, width, _ = img.shape
-			if height != width:
-				not_square.append([img_path, height, width])
-		if not_square:
-			print(not_square)
-			sys.exit(f'[\u2718] Not square images found for file {file_path}.')
-		
-		# print(f'Processed {file_path}.')
-	
-	
-	# read each json file
-	for file_path in json_files:
-		# read contents as json
-		base_dir = Path(file_path).parent
-		with open(file_path, 'r') as f:
-			try:
-				data = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
-			except ValueError as e:
-				sys.exit(f"[\u2718] Duplicate key '{e}' found in file {file_path}.")
-		
-		# ! removes empty keys
-		# data = {key: value for key, value in data.items() if key}
-		
-		# add attribute 'path' to each element
-		for key, value in data.items():
-			for i in range(len(value)):
-				value[i]['path'] = base_dir
-		
-		# run tests
-		if args.tests:
-			run_tests(data)
-		
-		# extend dictionary with new data
-		words.update(data)
-	
-	return words
-
-
-
-def get_wordlist(data: dict) -> List[str]:
-	return list(data.keys())
+from utils import *
 
 
 
@@ -188,12 +82,6 @@ def copy(src: str, dst: str, data: dict):
 
 
 
-def is_image(path: str):
-	_, ext = os.path.splitext(path)
-	return ext in ['.jpg', '.jpeg', '.png']
-
-
-
 def save(path: str, data: dict):
 	if not os.path.exists(path):
 		sys.exit(f'[\u2718] The path to moves images {path} does not exist.')
@@ -214,7 +102,7 @@ def save(path: str, data: dict):
 		shutil.move(src, dst)
 		
 		print(f'Successfully moved {file_name} with extension {ext}')
-	
+
 
 
 # add keyword arguments
@@ -230,19 +118,8 @@ args = parser.parse_args()
 
 
 if __name__ == '__main__':
-	base_dir = '../'
-	json_files = []
-	
-	# traverse all the files in root directory
-	folders = ['Common', 'Basic', 'Advanced']
-	for root, dirs, files in os.walk(base_dir):
-		for f in files:
-			if f.endswith('.json') and any(folder in root for folder in folders):
-				json_files.append(os.path.join(root, f))
-	
-	# get the wordlist
-	json_files.sort()
-	data = get_data(json_files)
+	# load data
+	data = get_data(args.tests)
 	
 	# search for word in wordlist
 	if args.word:
@@ -259,5 +136,3 @@ if __name__ == '__main__':
 	# moves files from the downloads folder and saves them
 	if args.save:
 		save(args.save, data)
-	
-	print('[\u2714] All checks passed!')
---
+++ scripts/utils.py
@@ -0,0 +1,156 @@
+import os
+import sys
+import cv2
+import json
+
+from typing import List
+from pprint import pprint
+from pathlib import Path
+
+
+
+# global variables
+root_dir = '/home/shoaib/Desktop/GREPrep/'
+folders = ['Common', 'Basic', 'Advanced']
+
+
+
+def dict_raise_on_duplicates(ordered_pairs):
+	""" Raises an error for duplicate keys """
+	d = {}
+	for k, v in ordered_pairs:
+		if k in d and k != "":	# ! ignores empty keys
+		   raise ValueError(k)
+		else:
+		   d[k] = v
+	return d
+
+
+
+def pretty(key, value):
+	print(f'{key}: ', end='')
+	pprint(value)
+
+
+
+def is_image(path: str):
+	_, ext = os.path.splitext(path)
+	return ext in ['.jpg', '.jpeg', '.png']
+
+
+
+def get_wordlist(data: dict) -> List[str]:
+	return list(data.keys())
+
+
+
+def get_data(is_test: bool = False) -> dict:
+	words = {}
+	json_files = []
+	
+	# traverse all the files in root directory
+	for root, dirs, files in os.walk(root_dir):
+		for f in files:
+			if f.endswith('.json') and any(folder in root for folder in folders):
+				json_files.append(os.path.join(root, f))
+	
+	# get the wordlist
+	json_files.sort()
+
+	
+	def run_tests(data: dict) -> None:
+		# check dictionary of data
+		for key, value in data.items():
+			if key in words:
+				pretty(key, words[key])
+				sys.exit(f"[\u2718] Duplicate key '{key}' found in file {file_path}.")
+		
+		# check if wordlist is sorted
+		pairs = []
+		keys = get_wordlist(data)
+		for i in range(1, len(keys)):
+			if keys[i - 1] > keys[i]:
+				pairs.append([keys[i - 1], keys[i]])
+		if pairs:
+			print(pairs)
+			sys.exit(f'[\u2718] Wordlist is not sorted for file {file_path}.')
+		
+		# check if image is present
+		images = os.listdir(os.path.join(base_dir, 'images'))
+		original = os.listdir(os.path.join(base_dir, 'original'))
+		no_image = []
+		for key, value in data.items():
+			found = False
+			for i in range(len(value)):
+				img_path = value[i]['image']
+				if img_path:
+					if img_path not in images or img_path not in original:
+						no_image.append(key)
+					found = True
+			if not found:
+				no_image.append(key)
+		if no_image:
+			print(no_image)
+			sys.exit(f'[\u2718] Incorrect path to images for file {file_path}.')
+		
+		# check if images are square
+		not_square = []
+		for img_path in images:
+			img = cv2.imread(os.path.join(base_dir, 'images', img_path))
+			height, width, _ = img.shape
+			if height != width:
+				not_square.append([img_path, height, width])
+		if not_square:
+			print(not_square)
+			sys.exit(f'[\u2718] Not square images found for file {file_path}.')
+		
+		# check if additional files present
+		extra_img = []
+		extra_org = []
+		for image in images:
+			file_name, ext = os.path.splitext(image)
+			if file_name not in data:
+				extra_img.append(image)
+		if extra_img:
+			print(extra_img)
+			sys.exit(f'[\u2718] Additional square images found in {file_path}.')
+		for image in original:
+			file_name, ext = os.path.splitext(image)
+			if file_name not in data:
+				extra_org.append(image)
+		if extra_org:
+			print(extra_org)
+			sys.exit(f'[\u2718] Additional original images found in {file_path}.')
+		
+		print(f'Processed {file_path}.')
+	
+	
+	# read each json file
+	for file_path in json_files:
+		# read contents as json
+		base_dir = Path(file_path).parent
+		with open(file_path, 'r') as f:
+			try:
+				data = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
+			except ValueError as e:
+				sys.exit(f"[\u2718] Duplicate key '{e}' found in file {file_path}.")
+		
+		# ! removes empty keys
+		# data = {key: value for key, value in data.items() if key}
+		
+		# add attribute 'path' to each element
+		for key, value in data.items():
+			for i in range(len(value)):
+				value[i]['path'] = base_dir
+		
+		# run tests
+		if is_test:
+			run_tests(data)
+		
+		# extend dictionary with new data
+		words.update(data)
+	
+	if is_test:
+		print('[\u2714] All checks passed!')
+	
+	return words
