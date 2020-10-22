cd49baa8e0a88d37aadcac0e88063be00433cc39
Mohammed-Shoaib <shoaib98libra@gmail.com>
2020-10-22 14:10:06 +0400

    [TS] Initial commit for price detail component

---
+++ Growcabulary/src/app/price-detail/price-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-price-detail',
+  templateUrl: './price-detail.component.html',
+  styleUrls: ['./price-detail.component.scss']
+})
+export class PriceDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
